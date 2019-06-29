32083ba455c33676a0b74c8241c8defbed72bf2f
Nima Soufiani <nimasoufiani@Nimas-MacBook-Pro.local>
2019-06-29 13:23:46 +0100

    File Name change

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from "./Screens/Home";
+import Home from "./Screens/HomeScreen";
 import { createStackNavigator, createAppContainer } from "react-navigation";
+import HomeScreen from './Screens/HomeScreen';
 const MainNavigator = createStackNavigator(
   //FIRST ARGUMENT: screens we want to switch to
     {
-  Home: {screen:Home} 
+  Home: {screen:HomeScreen} 
   }, 
   //SECOND ARGUMENT - what the default Navigation bar at the top of the phone should look like.
   {
